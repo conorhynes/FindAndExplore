b67e9c88aa55f18ac17f6a79eeb8eb90844d4e15
Hugh O'Higgins <hughohiggins@hotmail.com>
2013-05-18 13:53:34 +0100

    Added new scripts for enemy spawning and movement

--- DublinGameCraft/Assets/Enemy Movement/SpawnLevel.cs
+++ DublinGameCraft/Assets/Enemy Movement/SpawnLevel.cs
@@ -1,21 +1,33 @@
 using UnityEngine;
 using System.Collections;
 
-public class SpawnLevel : MonoBehaviour {
+public class SpawnLevel : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		for(int i = 0 ; i <10 ; i++)
-		{
-			float x = Random.Range (-100f, 100f);
-			float y = Random.Range (-100f, 100f);
-			//	Object ob = Instantiate (GameObject.FindGameObjectWithTag ("Finish"));
-			
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            float x = Random.Range(-10f, 10f);
+            float y = Random.Range(-10f, 10f);
+            GameObject ob = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            Vector3 pos = new Vector3(x, y, 0);
+            Rigidbody gameObjectsRigidBody = ob.AddComponent<Rigidbody>();
+            gameObjectsRigidBody.mass = 1;
+            gameObjectsRigidBody.useGravity = false;
+            ob.rigidbody.position = pos;
+            float xMove = Random.Range(-1f, 1f);
+            float yMove = Random.Range(-1f, 1f);
+            Vector3 direction = new Vector3(xMove, yMove, 0);
+            direction = direction.normalized * 50;
+            ob.rigidbody.AddForce(direction);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
