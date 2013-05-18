b39cf4cde45c1a60bd04d84d9d6c6b5be7df7b79
Hugh O'Higgins <hughohiggins@hotmail.com>
2013-05-18 16:30:46 +0100

    Added new scripts for enemy spawning and movement

--- DublinGameCraft/Assets/Enemy Movement/SpawnLevel.cs
+++ DublinGameCraft/Assets/Enemy Movement/SpawnLevel.cs
@@ -7,14 +7,35 @@ public class SpawnLevel : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        Vector3 wrapmax = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        Vector3 wrapmin = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, Camera.main.transform.position.z));
         for (int i = 0; i < 10; i++)
         {
-
-            float x = Random.Range(-10f, 10f);
-            float y = Random.Range(-10f, 10f);
+            float x;
+            float y;
+            if (i % 4 == 0)
+            {
+                x = wrapmax.x;
+                y = Random.Range(wrapmax.y, wrapmin.y);
+            }
+            else if (i % 4 == 1)
+            {
+                x = Random.Range(wrapmax.x, wrapmin.x);
+                y = wrapmin.y;
+            }
+            else if (i % 4 == 2)
+            {
+                x = wrapmin.x;
+                y = Random.Range(wrapmax.y, wrapmin.y);
+            }
+            else
+            {
+                x = Random.Range(wrapmax.x, wrapmin.x);
+                y = wrapmin.y;
+            }
 
             GameObject ob = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            Vector3 pos = new Vector3(x, 0, y);
+            Vector3 pos = new Vector3(x, y, 0);
             Rigidbody gameObjectsRigidBody = ob.AddComponent<Rigidbody>();
             gameObjectsRigidBody.mass = 1;
             gameObjectsRigidBody.useGravity = false;
