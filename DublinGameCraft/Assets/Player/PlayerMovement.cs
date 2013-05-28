578182f51598dca0110239a7b88daf3e24212b32
Hugh O'Higgins <hughohiggins@hotmail.com>
2013-05-28 13:12:12 +0100

    Player now screen wraps when you go off the screen

--- DublinGameCraft/Assets/Player/PlayerMovement.cs
+++ DublinGameCraft/Assets/Player/PlayerMovement.cs
@@ -18,7 +18,38 @@ public float MoveSpeed = 10;
 	 	float Vertical = Input.GetAxis("Vertical") * Time.deltaTime * MoveSpeed;
 
 	    // Move the player
-	    transform.Translate(Horizonal,0,Vertical);
+	    transform.Translate(Horizonal,Vertical,0);
 	 
 	}
+	
+	void OnBecameInvisible()
+    {
+        Vector3 pos = transform.position;
+        Vector3 wrapmin = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, Camera.main.transform.position.z));
+        Vector3 wrap = new Vector3(wrapmin.y - 0, wrapmin.x - 0);
+        Vector3 wrapmax = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        Vector3 wrapMax = new Vector3(wrapmax.y + 0, wrapmax.x + 0);
+
+        if (transform.position.x > wrapmin.x)
+        {
+            pos.x = wrapmax.x - 1;
+            //vel.x = vel.x * -1;
+        }
+        else if (transform.position.x < wrapmax.x)
+        {
+            pos.x = wrapmin.x + 1;
+            //vel.x = vel.x * -1;
+        }
+        if (transform.position.y > wrapmin.y)
+        {
+            pos.y = wrapmax.y - 1;
+            //vel.y = vel.y * -1;
+        }
+        else if (transform.position.y < wrapmax.y)
+        {
+            pos.y = wrapmin.y + 1;
+            //vel.y = vel.y * -1;
+        }
+        transform.position = pos;
+    }
 }
